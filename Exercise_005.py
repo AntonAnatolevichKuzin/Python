d320e9eef3e05171aa48a94247b3c1f57ddb5b78
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-15 01:12:39 +0500

    Exercise_005 preformed

--- Exercise_005.py
+++ Exercise_005.py
@@ -1,1 +1,15 @@
+#  Напишите программу, которая принимает на вход координаты двух точек и находит 
+#  расстояние между ними в 2D пространстве.
+    # *Пример:*
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
 
+xA = int(input('Input coordinate "X" 1-th point: '))
+yA = int(input('Input coordinate "Y" 1-th point: '))
+xB = int(input('Input coordinate "X" 2-th point: '))
+yB = int(input('Input coordinate "Y" 1-th point: '))
+
+print(f'A({xA},{yA}); B({xB},{yB}) -> ',end='')
+from math import sqrt
+distance = sqrt((xB-xA)**2 + (yB-yA)**2)
+print(round(distance, 3))

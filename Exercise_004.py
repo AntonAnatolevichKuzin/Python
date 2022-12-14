0b59a199fa51884ebaa1f13f07b3fa08664ca9e6
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-14 23:54:21 +0500

    Exercise_004 performed

--- Exercise_004.py
+++ Exercise_004.py
@@ -1,1 +1,15 @@
+# Напишите рограмму, которая по заданному номеру четверти, показывает 
+# диапазон возможных координат точек в этой четверти (x и y).
 
+num = int(input('Input number quorter: '))
+
+if num < 1 or num > 4:
+    print('Try again!')
+elif num == 1:
+    print(f'If quorter {num} then [x > 0; y > 0]')
+elif num == 2:
+    print(f'If quorter {num} then [x < 0; y > 0]')
+elif num == 3:
+    print(f'If quorter {num} then [x < 0; y < 0]')
+else:
+    print(f'If quorter {num} then [x > 0; y < 0]')

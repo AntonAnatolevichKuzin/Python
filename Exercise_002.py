2c11d3691a9b1d6fa5604fcc4d946f3fcde66a1b
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-14 23:09:19 +0500

    Exercise_002 performed

--- Exercise_002.py
+++ Exercise_002.py
@@ -1,1 +1,13 @@
+#Напишите программу для. проверки истинности утверждения 
+#  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+x = int(input('Enter X value: '))
+y = int(input('Enter Y value: '))
+z = int(input('Enter Z value: '))
+
+result = not(x and y and z) == (not (x) or not (y) or not (z))
+
+if result == True:
+    print('True')
+else:
+    print('Not true')
